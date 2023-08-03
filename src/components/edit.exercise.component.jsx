571ae636efaebc064e9b8a6cf5c930e7d776a135
Jon Formantes <36646177+matui3@users.noreply.github.com>
2023-08-02 17:34:58 -0700

    Learning MERN Stack

---
+++ src/components/edit.exercise.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EditExercises() {
+    return (
+        <div>
+            <p>You are on the Edit Exercises Component!</p>
+        </div>
+    )
+}
+
+export default EditExercises;
---
+++ src/components/exercisises-list.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ExerciseList() {
+    return (
+        <div>
+            <p>You are on the Exercises List Component!</p>
+        </div>
+    )
+}
+
+export default ExerciseList;
